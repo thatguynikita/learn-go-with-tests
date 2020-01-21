fcc4629054e9cc6c5fe43680a6d2f6dd67a493f7
Nikita Chernozipunnikov <ziperson@gmail.com>
2020-01-21 16:35:00 +0300

    Integers complete

---
+++ integers/adder_test.go
@@ -0,0 +1,31 @@
+package integers
+
+import "testing"
+
+import "fmt"
+
+func TestAdd(t *testing.T) {
+	t.Run("2 + 2 = 4 check", func(t *testing.T) {
+		sum := Add(2, 2)
+		expected := 4
+	
+		if sum != expected {
+			t.Errorf("expected %d, got %d", expected, sum)
+		}
+	})
+
+	t.Run("Zero check", func(t *testing.T) {
+		sum := Add(0, 0)
+		expected := 0
+	
+		if sum != expected {
+			t.Errorf("expected %d, got %d", expected, sum)
+		}
+	})
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
