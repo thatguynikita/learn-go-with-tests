f5e9a80123d2444629932a3dcce389ef01b3aecb
Nikita Chernozipunnikov <ziperson@gmail.com>
2020-01-24 13:15:25 +0300

    Context - passing the context in the arguments instead

--- context/context.go
+++ context/context.go
@@ -1,32 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 // Store interface for our store
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
 
 // Server is a handler that fetches the store
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+		data, err := store.Fetch(r.Context())
 
-		data := make(chan string, 1)
-
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(w, d)
-		case <-ctx.Done():
-			store.Cancel()
+		if err != nil {
+			return // todo: log error however you like
 		}
+
+		fmt.Fprintf(w, data)
 	}
 }
